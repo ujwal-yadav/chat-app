d584cd118ed0c35878ceb3f10522de3d65bc8b2c
Ujwal Kumar Yadav <ujwalofficial99@gmail.com>
2022-10-16 00:55:21 +0530

    Delete frontend directory

--- frontend/src/utils/APIRoutes.js
+++
@@ -1,7 +0,0 @@
-export const host = "http://localhost:8000";
-export const signupRoute = `${host}/api/auth/signup`;
-export const loginRoute = `${host}/api/auth/login`;
-export const setAvatarRoute = `${host}/api/auth/setAvatar`;
-export const allUsersRoute = `${host}/api/auth/allusers`;
-export const sendMessageRoute = `${host}/api/messages/addmsg`;
-export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
